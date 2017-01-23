d9c7875e39e4b82cc7ac068d87904b8253c348a0
Minh Nguyen <minhnguyen@Minhs-iMac.local>
2017-01-23 16:38:10 +0700

    add project

---
+++ app/src/main/java/com/example/minhnguyen/notetakingapp/DBOpenHelper.java
@@ -0,0 +1,53 @@
+package com.example.minhnguyen.notetakingapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by minhnguyen on 1/23/17.
+ */
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+
+
+    //Constants for db name and version
+    private static final String DATABASE_NAME = "notes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Constants for identifying table and columns
+    public static final String TABLE_NOTES = "notes";
+    public static final String NOTE_ID = "_id";
+    public static final String NOTE_TEXT = "noteText";
+    public static final String NOTE_CREATED = "noteCreated";
+
+    //SQL to create table
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NOTES + " (" +
+                    NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    NOTE_TEXT + " TEXT, " +
+                    NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP" +
+                    ")";
+
+    //SQL to drop table
+    private static final String TABLE_DROP =
+            "DROP TABLE IF EXIST" + TABLE_NOTES;
+
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(TABLE_DROP);
+        onCreate(db);
+    }
+
+
+}
